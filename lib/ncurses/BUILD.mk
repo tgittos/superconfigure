bef6a7d5266e42652693ce0b255b8b8b5124bbfc
Tim Gittos <tim.gittos@bluetongue.ai>
2024-10-25 12:41:27 +0000

    Trying to get ncurses crossplatform compiling

--- lib/ncurses/BUILD.mk
+++ lib/ncurses/BUILD.mk
@@ -1,6 +1,6 @@
 NCURSES_SRC := https://ftp.gnu.org/gnu/ncurses/ncurses-6.4.tar.gz
 
-NCURSES_CONFIG_ARGS = --without-libtool --without-shared\
+NCURSES_X86_64_CONFIG_ARGS = --without-libtool --without-shared\
     --without-manpages --without-tests --without-tack\
     --without-ada --without-cxx --without-cxx-binding\
     --without-tests --with-termlib --with-ticlib\
@@ -11,9 +11,12 @@ NCURSES_CONFIG_ARGS = --without-libtool --without-shared\
     --sysconfdir=/zip --datarootdir=/zip/usr/share\
     CFLAGS="-Os"
 
+NCURSES_AARCH64_CONFIG_ARGS = $(NCURSES_X86_64_CONFIG_ARGS) --build=aarch64
 
 $(eval $(call DOWNLOAD_SOURCE,lib/ncurses,$(NCURSES_SRC)))
-# no deps for ncurses except cosmo repo
-$(eval $(call AUTOTOOLS_BUILD,lib/ncurses,$(NCURSES_CONFIG_ARGS),$(NCURSES_CONFIG_ARGS)))
+# build calls bash during tinfo build, need a cosmo bash
+# this gets downloaded by the cosmo script
+$(eval $(call SPECIFY_DEPS,cli/mawk))
+$(eval $(call AUTOTOOLS_BUILD,lib/ncurses,$(NCURSES_X86_64_CONFIG_ARGS),$(NCURSES_AARCH64_CONFIG_ARGS)))
 
 o/lib/ncurses/built.fat: FATTEN_COMMAND = $(DUMMYLINK0)
